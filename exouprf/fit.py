7e952c4c72223e9c45272e75c8bc095fe1f5480b
radicamc <radica@astro.umontreal.ca>
2024-05-24 10:41:08 -0400

    Initial code

---
+++ exouprf/fit.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 09:38 2024
+
+@author: MCR
+
+Stuff.
+"""
+
+import copy
+import emcee
+import numpy as np
+
+from exouprf.model import Model
+
+
+class Dataset:
+
+    def __init__(self, input_parameters, t, linear_regressors=None,
+                 observations=None, gp_regressors=None, ld_model='quadratic',
+                 silent=False):
+
+        self.t = t
+        self.ld_model = ld_model
+        self.flux_decomposed = None
+        self.flux = None
+        self.observations = observations
+        self.linear_regressors = linear_regressors
+        self.gp_regressors = gp_regressors
+        self.pl_params = input_parameters
+        self.mcmc_sampler = None
+        self.silent = silent
+
+        for param in self.pl_params:
+            dist = self.pl_params[param]['distribution']
+            if dist == 'fixed':
+                self.pl_params[param]['function'] = None
+            elif dist == 'uniform':
+                self.pl_params[param]['function'] = logprior_uniform
+            else:
+                msg = 'Unknown distribution {0} for parameter ' \
+                      '{1}'.format(dist, param)
+                raise ValueError(msg)
+
+    def fit(self, sampler='mcmc', mcmc_start=None, mcmc_steps=10000,
+            silent=False):
+
+        if sampler == 'mcmc':
+            msg = 'Starting positions must be provided for MCMC sampling.'
+            assert mcmc_start is not None, msg
+
+            log_prob_args = (self.pl_params, self.t, self.observations,
+                             self.linear_regressors, self.gp_regressors,
+                             self.ld_model)
+            mcmc_sampler = fit_emcee(mcmc_start, log_probability,
+                                     silent=silent, mcmc_steps=mcmc_steps,
+                                     log_probability_args=log_prob_args)
+            self.mcmc_sampler = mcmc_sampler
+
+
+def fit_emcee(initial_pos, log_prob, silent=False, mcmc_steps=10000,
+              log_probability_args=None):
+
+    nwalkers, ndim = initial_pos.shape
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob,
+                                    args=log_probability_args)
+    output = sampler.run_mcmc(initial_pos, mcmc_steps, progress=not silent)
+
+    return sampler
+
+
+def set_logprior(theta, param_dict):
+
+    logprior = 0
+    pcounter = 0
+    for param in param_dict:
+        if param_dict[param]['distribution'] == 'fixed':
+            continue
+        v1, v2 = param_dict[param]['value']
+        thisprior = param_dict[param]['function'](theta[pcounter], v1, v2)
+        logprior += thisprior
+        pcounter += 1
+
+    return logprior
+
+
+def log_likelihood(theta, param_dict, time, observations,
+                   linear_regressors=None, gp_regressors=None,
+                   ld_model='quadratic'):
+
+    log_like = 0
+    pcounter = 0
+    for param in param_dict:
+        if param_dict[param]['distribution'] == 'fixed':
+            continue
+        param_dict[param]['value'] = (theta[pcounter])
+        pcounter += 1
+
+    thismodel = Model(param_dict, time, linear_regressors=linear_regressors,
+                      observations=observations, gp_regressors=gp_regressors,
+                      ld_model=ld_model, silent=True)
+    thismodel.compute_lightcurves()
+    for inst in observations.keys():
+        mod = thismodel.flux_decomposed[inst]['total']
+        dat = observations[inst]['flux']
+        t = time[inst]
+        err = param_dict['sigma_{}'.format(inst)]['value']
+        log_like -= 0.5 * np.log(2 * np.pi * err ** 2) * len(t)
+        log_like -= 0.5 * np.sum((dat - mod) ** 2 / err ** 2)
+
+    return log_like
+
+
+def log_probability(theta, param_dict, time, observations,
+                    linear_regressors=None, gp_regressors=None,
+                    ld_model='quadratic'):
+
+    lp = set_logprior(theta, param_dict)
+    if not np.isfinite(lp):
+        return -np.inf
+    ll = log_likelihood(theta, copy.deepcopy(param_dict), time, observations,
+                        linear_regressors, gp_regressors, ld_model)
+    log_prob = lp + ll
+
+    return log_prob
+
+
+def logprior_uniform(x, low_bound, up_bound):
+
+    if low_bound <= x <= up_bound:
+        return np.log(1 / (up_bound - low_bound))
+    else:
+        return -np.inf
---
+++ exouprf/model.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 09:36 2024
+
+@author: MCR
+
+Stuff.
+"""
+
+import batman
+import numpy as np
+
+
+class Model:
+
+    def __init__(self, input_parameters, t, linear_regressors=None,
+                 observations=None, gp_regressors=None, ld_model='quadratic',
+                 silent=False):
+
+        self.t = t
+        self.ld_model = ld_model
+        self.flux_decomposed = None
+        self.flux = None
+        self.observations = observations
+        self.linear_regressors = linear_regressors
+        self.gp_regressors = gp_regressors
+        self.silent = silent
+
+        # Go through input params once to get number of different instruments.
+        self.multiplicity = {}
+        for param in input_parameters.keys():
+            param_split = param.split('_')
+            # Length of param_split should be at least 3 -- key name, planet
+            # number, and instrument.
+            # First chunk is always parameter key, so start at chunk number 2.
+            for chunk in param_split[1:]:
+                # Ignore planet identifiers.
+                if chunk[0] == 'p' and chunk[1].isdigit():
+                    pass
+                # If it is a new instrument, add to list.
+                elif chunk not in self.multiplicity.keys():
+                    self.multiplicity[chunk] = []
+                    # Make sure time axis is passed for each instrument.
+                    if chunk not in t.keys():
+                        msg = 'No timestamps passed for instrument ' \
+                              '{}'.format(chunk)
+                        raise ValueError(msg)
+
+        # Now go through a second time to get the number of planets.
+        for param in input_parameters.keys():
+            param_split = param.split('_')
+            for inst in self.multiplicity.keys():
+                if inst in param_split:
+                    for chunk in param_split[1:]:
+                        # If it is a new planet identifier, increase
+                        # multiplicity.
+                        if chunk[0] == 'p' and chunk[1].isdigit():
+                            if chunk not in self.multiplicity[inst]:
+                                self.multiplicity[inst].append(chunk)
+
+        for inst in self.multiplicity.keys():
+            if not self.silent:
+                print('Importing parameters for {0} planet(s) from instrument '
+                      '{1}.'.format(len(self.multiplicity[inst]), inst))
+
+        # Set up storage dictionaries for properties of each planet.
+        self.pl_params = {}
+        for inst in self.multiplicity.keys():
+            self.pl_params[inst] = {}
+            for pl in self.multiplicity[inst]:
+                self.pl_params[inst][pl] = {}
+
+        # Populate parameters dictionary from input data.
+        for param in input_parameters.keys():
+            param_split = param.split('_')
+            # First chunk is always parameter key.
+            prop = param_split[0]
+            # Keys for physical planet parameters.
+            if prop in ['per', 't0', 'rp', 'a', 'inc', 'u1', 'u2', 'u3', 'u4',
+                        'ecc', 'w']:
+                # Add to correct instrument and planet dictionary.
+                for inst in self.multiplicity.keys():
+                    if inst in param_split:
+                        for pl in self.multiplicity[inst]:
+                            if pl in param_split:
+                                self.pl_params[inst][pl][prop] = input_parameters[param]['value']
+            # Error inflation parameter -- property of instrument.
+            elif prop == 'sigma':
+                for inst in self.multiplicity.keys():
+                    if inst in param_split:
+                        self.pl_params[inst][prop] = input_parameters[param]['value']
+            # Linear systematics -- property of instrument.
+            elif prop[:5] == 'theta':
+                for inst in self.multiplicity.keys():
+                    if inst in param_split:
+                        self.pl_params[inst][prop] = input_parameters[param]['value']
+            else:
+                msg = 'Unrecognized input parameter: {}'.format(param)
+                raise ValueError(msg)
+
+    def compute_lightcurves(self):
+
+        if not self.silent:
+            print('Computing lighcturves for all instruments.')
+        self.flux_decomposed = {}
+        self.flux_decomposed = {}
+        self.flux = {}
+
+        # Individually treat each instrument.
+        for inst in self.multiplicity.keys():
+            self.flux_decomposed[inst] = {}
+            self.flux_decomposed[inst]['pl'] = {}
+            self.flux[inst] = np.ones_like(self.t[inst])
+            lm, gp = False, False
+
+            # Generate astrophysical light curve for each planet.
+            for pl in self.multiplicity[inst]:
+                ld_params = []
+                for param in self.pl_params[inst][pl].keys():
+                    if param in ['u1', 'u2', 'u3', 'u4']:
+                        ld_params.append(self.pl_params[inst][pl][param])
+                pl_flux = batman_transit(self.t[inst],
+                                         self.pl_params[inst][pl]['t0'],
+                                         self.pl_params[inst][pl]['per'],
+                                         self.pl_params[inst][pl]['rp'],
+                                         self.pl_params[inst][pl]['a'],
+                                         self.pl_params[inst][pl]['inc'],
+                                         self.pl_params[inst][pl]['ecc'],
+                                         self.pl_params[inst][pl]['w'],
+                                         ld_params,
+                                         ld_model=self.ld_model)
+                self.flux_decomposed[inst]['pl'][pl] = pl_flux
+                self.flux[inst] -= (1 - pl_flux)
+            self.flux_decomposed[inst]['pl']['total'] = np.copy(self.flux[inst])
+
+            # Add in linear systematics model, if any.
+            self.flux_decomposed[inst]['lm'] = None
+            # Unpack multipliers.
+            thetas = []
+            for param in self.pl_params[inst].keys():
+                if param[:5] == 'theta':
+                    msg = 'No regressors passed for instrument {}'.format(inst)
+                    assert inst in self.linear_regressors.keys(), msg
+                    thetas.append(self.pl_params[inst][param])
+                    # Note that we want to add linear models.
+                    lm = True
+
+            if lm is True:
+                if not self.silent:
+                    print('Linear model(s) detected for instrument '
+                          '{}.'.format(inst))
+                self.flux_decomposed[inst]['lm'] = {}
+                regressors = np.array(self.linear_regressors[inst])
+                # Make sure that the number of regressors equals the number of
+                # lm parameters.
+                msg = 'Number of linear model parameters does not match ' \
+                      'number of regressors for instrument {}.'.format(inst)
+                assert np.shape(regressors)[0] == len(thetas), msg
+                self.flux_decomposed[inst]['lm']['total'] = np.zeros_like(regressors[0])
+                for i, theta in enumerate(thetas):
+                    thismodel = theta * regressors[i]
+                    self.flux_decomposed[inst]['lm']['regressor{}'.format(i)] = thismodel
+                    self.flux_decomposed[inst]['lm']['total'] += thismodel
+
+                self.flux[inst] += self.flux_decomposed[inst]['lm']['total']
+
+            self.flux_decomposed[inst]['total'] = self.flux[inst]
+
+    def simulate_observations(self):
+
+        # Make sure that light curves have already been calculated.
+        if self.flux_decomposed is None:
+            msg = 'It looks like the compute_lightcurves method has not yet ' \
+                  'been run.\n compute_lightcurves must be run before ' \
+                  'create_observations.'
+            raise ValueError(msg)
+
+        # Don't run if observations already exist.
+        if self.observations is not None:
+            msg = 'Observational data already exists. I imagine you do not ' \
+                  'want to overwrite it!'
+            raise ValueError(msg)
+
+        if not self.silent:
+            print('Simulating observations for all instruments.')
+
+        # Add scatter to light curves.
+        self.observations = {}
+        for inst in self.multiplicity.keys():
+            self.observations[inst] = {}
+            jitter = self.pl_params[inst]['sigma']
+            flux_jitter = np.random.normal(self.flux[inst], scale=jitter)
+            self.observations[inst]['flux'] = flux_jitter
+            self.observations[inst]['flux_err'] = jitter
+
+
+def batman_transit(t, t0, per, rp, a, inc, ecc, w, ld, ld_model='quadratic'):
+
+    params = batman.TransitParams()
+    params.t0 = t0
+    params.per = per
+    params.rp = rp
+    params.a = a
+    params.inc = inc
+    params.ecc = ecc
+    params.w = w
+    params.limb_dark = ld_model
+    params.u = ld
+
+    m = batman.TransitModel(params, t)
+    flux = m.light_curve(params)
+
+    return flux
