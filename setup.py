276ac4502f0b8794295858a2ffcc38fb19f6286b
radicamc <radicamc@uchicago.edu>
2026-08-07 09:16:49 -0500

    Stage v1.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='exouprf',
-      version='1.1.1',
+      version='1.1.2',
       license='MIT',
       author='Michael Radica',
       author_email='radicamc@uchicago.edu',
